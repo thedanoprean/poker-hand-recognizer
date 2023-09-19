52883d8119476e08c7d544b82b4ccd9e810d7d5c
DanOprean <dan.oprean.77@gmail.com>
2023-09-19 13:16:36 +0300

    Adding first files

---
+++ PokerWebcam.py
@@ -0,0 +1,63 @@
+from ultralytics import YOLO
+import cv2
+import cvzone
+import math
+import PokerFunction
+
+cap = cv2.VideoCapture(0)  # Webcam
+# cap = cv2.VideoCapture("../Videos/1.mp4") # Video
+cap.set(3, 1280)
+cap.set(4, 720)
+
+model = YOLO("pokerCards.pt")
+
+classNames = ['10C', '10D', '10H', '10S',
+              '2C', '2D', '2H', '2S',
+              '3C', '3D', '3H', '3S',
+              '4C', '4D', '4H', '4S',
+              '5C', '5D', '5H', '5S',
+              '6C', '6D', '6H', '6S',
+              '7C', '7D', '7H', '7S',
+              '8C', '8D', '8H', '8S',
+              '9C', '9D', '9H', '9S',
+              'AC', 'AD', 'AH', 'AS',
+              'JC', 'JD', 'JH', 'JS',
+              'KC', 'KD', 'KH', 'KS',
+              'QC', 'QD', 'QH', 'QS'
+              ]
+
+
+while True:
+    success, img = cap.read()
+    results = model(img, stream=True)
+    hand = []
+    for r in results:
+        boxes = r.boxes
+        for box in boxes:
+            # Bounding Box
+            x1, y1, x2, y2 = box.xyxy[0]
+            x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
+            w, h = x2 - x1, y2 - y1
+            bbox = int(x1), int(y1), int(w), int(h)
+            cvzone.cornerRect(img, bbox)
+
+            # Confidence
+            confidence = math.ceil((box.conf[0] * 100)) / 100
+
+            # Class Name
+            cls = int(box.cls[0])
+            cvzone.putTextRect(img, f'{classNames[cls]} {confidence}', (max(0, x1), max(40, y1)), scale=0.8,
+                               thickness=1)
+            if confidence > 0.5:
+                hand.append(classNames[cls])
+
+    hand = list(set(hand))
+    print(hand)
+    if len(hand) == 5:
+        results = PokerFunction.findPokerHand(hand)
+        print(results)
+        cvzone.putTextRect(img, f'Your Hand: {results}', (300, 75), scale=3,
+                           thickness=5)
+
+    cv2.imshow("Image", img)
+    cv2.waitKey(1)
